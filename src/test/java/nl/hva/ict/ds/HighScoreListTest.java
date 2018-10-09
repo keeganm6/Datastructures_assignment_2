da74a87b406166c545519c5c50b51ecbd8150b42
K.C. Meijer <keegan.meijer@hva.nl>
2018-10-09 19:17:32 +0200

    Initial commit

---
+++ src/test/java/nl/hva/ict/ds/HighScoreListTest.java
@@ -0,0 +1,83 @@
+package nl.hva.ict.ds;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.security.SecureRandom;
+import java.util.Random;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * This class contains some unit tests. They by no means ensure that all the requirements are implemented
+ * correctly.
+ */
+public class HighScoreListTest {
+    private static final int MAX_HIGH_SCORE = 100000;
+    private Random randomizer = new SecureRandom();
+    private HighScoreList highScores;
+    private Player nearlyHeadlessNick;
+    private Player dumbledore;
+
+    @Before
+    public void setup() {
+        // Here you should select your implementation to be tested.
+        highScores = new DummyHighScores();
+//        highScores = new InsertionSortHighScores();
+//        highScores = new BucketSortHighScores();
+//        highScores = new PriorityQueueHighScores();
+
+        nearlyHeadlessNick = new Player("Nicholas", "de Mimsy-Porpington", getHighScore() % 200);
+        dumbledore = new Player("Albus", "Dumbledore", nearlyHeadlessNick.getHighScore() * 1000);
+    }
+
+    @Test
+    public void noPlayerNoHighScore() {
+        assertTrue("There are high-score while there should be no high-scores!", highScores.getHighScores(1).isEmpty());
+    }
+
+    @Test
+    public void whenNoHighScoreIsAskedForNonShouldBeGiven() {
+        highScores.add(dumbledore);
+
+        assertEquals(0, highScores.getHighScores(0).size());
+    }
+
+    @Test
+    public void noMoreHighScoresCanBeGivenThenPresent() {
+        highScores.add(nearlyHeadlessNick);
+        highScores.add(dumbledore);
+
+        assertEquals(2, highScores.getHighScores(10).size());
+    }
+
+    @Test
+    public void keepAllHighScores() {
+        highScores.add(nearlyHeadlessNick);
+        highScores.add(dumbledore);
+
+        assertEquals(2, highScores.getHighScores(2).size());
+    }
+
+    @Test
+    public void singlePlayerHasHighScore() {
+        highScores.add(dumbledore);
+
+        assertEquals(dumbledore, highScores.getHighScores(1).get(0));
+    }
+
+    @Test
+    public void harryBeatsDumbledore() {
+        highScores.add(dumbledore);
+        Player harry = new Player("Harry", "Potter", dumbledore.getHighScore() + 1);
+
+        assertEquals(harry, highScores.getHighScores(1).get(0));
+    }
+
+    // Extra unit tests go here
+
+    private long getHighScore() {
+        return randomizer.nextInt(MAX_HIGH_SCORE);
+    }
+}
