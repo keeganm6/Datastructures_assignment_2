c7520ac99d2c9e2eb1224a484dd0578378cbfefb
Joey Blankendaal <joeyblankendaal@gmail.com>
2018-10-09 20:28:28 +0200

    Ws niet goed maar dit is het beste wat ik kan bedenken voor nu

--- src/main/java/nl/hva/ict/ds/PriorityQueueHighScores.java
+++ src/main/java/nl/hva/ict/ds/PriorityQueueHighScores.java
@@ -1,7 +1,9 @@
 package nl.hva.ict.ds;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.PriorityQueue;
 
 /**
  * Use a priority queue
@@ -9,19 +11,37 @@ import java.util.List;
  * @author Joey Blankendaal (500778751)
  */
 public class PriorityQueueHighScores implements HighScoreList, Comparator<Player> {
+    private List<Player> players = new ArrayList<>();
+    
     @Override
     public void add(Player player) {
-        
+        players.add(player);
     }
-
+    
     @Override
     public List<Player> getHighScores(int numberOfHighScores) {
+        PriorityQueue<Player> queue = new PriorityQueue<>(numberOfHighScores, this);
+        List<Player> sortedPlayers = new ArrayList<>();
         
+        while (queue.size() != 0) {
+            sortedPlayers.add(queue.remove());
+        }
+        
+        return sortedPlayers;
     }
-
+    
+    // Rewrite dit later maar
     @Override
-    public List<Player> findPlayer(String firstName, String lastName) throws IllegalArgumentException {
+    public List<Player> findPlayer(String firstName, String lastName) {
+        List<Player> matchedPlayers = new ArrayList<>();
         
+        for (Player player : players) {
+            if (player.getFirstName().equals(firstName) && player.getLastName().equals(lastName)) {
+                matchedPlayers.add(player);
+            }
+        }
+        
+        return matchedPlayers;
     }
 
     @Override
